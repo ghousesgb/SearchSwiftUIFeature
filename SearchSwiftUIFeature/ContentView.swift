b7a8020d7a9595e032f5b88a34f96619b613a145
Shaik Ghouse Basha <ShaikGhouse.Basha@geappliances.com>
2020-10-15 18:26:13 +0530

    Initial Commit

---
+++ SearchSwiftUIFeature/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchSwiftUIFeature
+//
+//  Created by Shaik Ghouse Basha on 15/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SearchSwiftUIFeature/SearchSwiftUIFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchSwiftUIFeatureApp.swift
+//  SearchSwiftUIFeature
+//
+//  Created by Shaik Ghouse Basha on 15/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SearchSwiftUIFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
